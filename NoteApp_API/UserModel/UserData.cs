917accd2da404cbf311d4c0f05d76e40fbfaa4e5
Michael <michaeljohntupas@gmail.com>
2022-05-09 11:34:20 +0800

    Update 050920221134

---
+++ NoteApp_API/UserModel/UserData.cs
@@ -0,0 +1,9 @@
+﻿namespace NoteApp_API.UserModel
+{
+    public class UserData
+    {
+        public string UserName { get; set; } = string.Empty;
+        public byte[] PasswordHash { get; set; }
+        public byte[] PasswordSalt { get; set; }
+    }
+}
